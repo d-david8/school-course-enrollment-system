db746f78bfd70bec8856541f44b029851eea1a30
Dan David <d_david8@yahoo.com>
2023-12-02 21:02:43 +0200

    create get all students api

--- src/main/java/ro/ddavid8/schoolcourseenrollmentsystem/services/StudentService.java
+++ src/main/java/ro/ddavid8/schoolcourseenrollmentsystem/services/StudentService.java
@@ -2,8 +2,12 @@ package ro.ddavid8.schoolcourseenrollmentsystem.services;
 
 import ro.ddavid8.schoolcourseenrollmentsystem.models.dtos.StudentDTO;
 
+import java.util.List;
+
 public interface StudentService {
     public StudentDTO createStudent(StudentDTO studentDTO);
 
     public StudentDTO updateStudent(Long studentId, StudentDTO studentDTO);
+
+    public List<StudentDTO> getAllStudents();
 }
